52598c9d8c70612eaad8173c1f29945e1171305c
Phil Lisak <phil.lisak@gmail.com>
2020-06-12 19:37:38 -0700

    Getting NaN on prototype return, something must be wrong with my functions...

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,15 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: Because they are enclosed within the same function and on the same scope.
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+// function summation(number) {
+// for (let i = 0; i <= number; i++) {
+  
+// }
+// }
